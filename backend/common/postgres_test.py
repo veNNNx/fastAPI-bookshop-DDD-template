1e14b1bd32537c8dae6477d4dda337e82eb8acdf
veNNNx <piotrek.cwalina1@wp.pl>
2025-08-07 13:12:38 +0200

    Testing CI

--- backend/common/postgres_test.py
+++ backend/common/postgres_test.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 
 TEST_DATABASE_URL = "postgresql://root:password@localhost:5432/test_postgres"
 
-test_engine = create_engine(TEST_DATABASE_URL)
+test_engine = create_engine(TEST_DATABASE_URL, isolation_level="AUTOCOMMIT")
 
 
 TestingSessionLocal = sessionmaker(
